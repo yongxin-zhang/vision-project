24e3216e56309d279bddb1b206a68a44b3a3b775
yongxinzhang <yongxinzhang@local>
2018-07-31 05:35:23 +0000

    yongxin's change with conv2D, Max/Averagepooling and dropout

--- train_emotion_classifier.py
+++ train_emotion_classifier.py
@@ -1,4 +1,4 @@
-from keras.layers import Dense, Flatten
+from keras.layers import Dense, Flatten, Conv2D, Dropout, MaxPooling2D, AveragePooling2D
 from keras.models import Sequential
 from keras.callbacks import Callback
 import pandas as pd
@@ -17,6 +17,12 @@ config = run.config
 
 config.batch_size = 32
 config.num_epochs = 20
+
+config.first_layer_conv_width = 3
+config.first_layer_conv_height = 3
+
+config.dropout = 0.4 # 0.2 didn't work well to avoid overfit, when 0.4 works!
+config.hidden_nodes = 2000 # 200 could only improved val_acc to ~53%, when 2000 made it further to 78%!
 
 input_shape = (48, 48, 1)
 
@@ -53,7 +59,26 @@ train_faces /= 255.
 val_faces /= 255.
 
 model = Sequential()
+
+# Expr. 1, with conv2D + Maxpooling + dropout0.2, we get ~59% val_acc.
+model.add(Conv2D(32,
+    (config.first_layer_conv_width, config.first_layer_conv_height),
+    input_shape=(48, 48,1),
+    activation='relu'))
+model.add(AveragePooling2D(pool_size=(2, 2))) # Averagepooling didn't add much on val_acc, still around 60%
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64,
+    (config.first_layer_conv_width, config.first_layer_conv_height),
+    input_shape=(48, 48,1),
+    activation='relu'))
+model.add(AveragePooling2D(pool_size=(3, 3)))
+#model.add(MaxPooling2D(pool_size=(3, 3)))
+
 model.add(Flatten(input_shape=input_shape))
+model.add(Dropout(config.dropout))
+model.add(Dense(config.hidden_nodes, activation='relu'))
+model.add(Dropout(config.dropout))
 model.add(Dense(num_classes, activation="softmax"))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy',
